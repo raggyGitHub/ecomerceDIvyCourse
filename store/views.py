967579e4a7d5224ca834482178465368faf659ff
ronald sarmiento <ronisarmont@gmail.com>
2020-05-26 15:20:56 -0400

    add to cart functionality part 1 update item create ordr

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
 from .models import *
 
 # Create your views here.
@@ -29,3 +32,26 @@ def checkout(request):
         order = {'get_cart_total':0,'get_cart_items':0}
     context = {'items':items,'order':order}
     return render (request,'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data ['productId']
+    action = data ['action']
+
+    print('Action:',action)
+    print('Product Id:',productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id = productId)
+    order, created = Order.objects.get_or_create(customer = customer, complete = False )
+
+    orderItem, created = OrderItem.objects.get_or_create(order = order , product = product )
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1 )
+    elif action =='remove':
+        orderItem.quantity = (orderItem.quantity - 1 )
+    
+    orderItem.save()
+    if orderItem.quantity <= 0:
+        orderItem.delete() 
+    return JsonResponse('Item was added ', safe = False)
